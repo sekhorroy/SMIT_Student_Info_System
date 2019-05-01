b0e4bc58fb8236c25e349da52dc8771e2f491095
shekhorroy100@gmail.com <16242589+sekhorroy@users.noreply.github.com>
2019-05-01 20:49:26 +0530

    v-0.1

--- server/models/FeeDepartment.js
+++ server/models/FeeDepartment.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const FeeDepartmentSchema = new mongoose.Schema({
   empId: {
--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   regNo :{
